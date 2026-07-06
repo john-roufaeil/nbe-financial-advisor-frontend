d7d5a47d2d20987e72396241c308e9c593d63261
john-roufaeil <john.f.roufaeil@gmail.com>
2026-07-07 01:04:34 +0300

    chore: init vite react-ts project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import babel from "vite-plugin-babel";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    babel({
+      filter: /\.[jt]sx?$/,
+      babelConfig: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
 });
