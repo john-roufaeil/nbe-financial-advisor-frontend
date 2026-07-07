062f5a4aaa655b6aa9753de254064926b9f4d10d
john-roufaeil <john.f.roufaeil@gmail.com>
2026-07-07 13:41:36 +0300

    test

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -8,6 +8,12 @@ export default function Dashboard() {
         <div className="card-body">
           <h1 className="card-title">{t("nav.err")}</h1>
           <button className="btn btn-primary w-fit">{t("nav.dashboard")}</button>
+          <button className="btn btn-secondary w-fit">{t("nav.dashboard")}</button>
+          <button className="btn btn-accent w-fit">{t("nav.dashboard")}</button>
+          <button className="btn btn-active w-fit">{t("nav.dashboard")}</button>
+          <button className="btn btn-ghost w-fit">{t("nav.dashboard")}</button>
+          <button className="btn btn-info w-fit">{t("nav.dashboard")}</button>
+          <button className="btn btn-link w-fit">{t("nav.dashboard")}</button>
         </div>
       </div>
     </div>
